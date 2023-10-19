bf2440bf0fb4b650c0a9d5ad05fd5ea421659ffb
Jake Ferrari <jakeferrari93@gmail.com>
2023-10-18 20:13:39 -0700

    update lsp & linters

--- lua/gtxferrari/plugins/lsp/mason.lua
+++ lua/gtxferrari/plugins/lsp/mason.lua
@@ -27,16 +27,17 @@ return {
 		mason_lspconfig.setup({
 			-- list of servers for mason to install
 			ensure_installed = {
-				"tsserver",
-				"html",
-				"cssls",
-				"tailwindcss",
-				"svelte",
-				"lua_ls",
-				"graphql",
-				"emmet_ls",
-				"prismals",
-				"pyright",
+				"bashls", -- Bash
+				"cssls", -- CSS
+				"html", -- HTML
+				"lua_ls", -- Lua
+				"marksman", -- Markdown
+				"powershell_es", -- Powershell
+				"prismals", -- TypeScript, Javascript
+				"pyright", -- Python
+				"svelte", -- Javascript
+				"tailwindcss", -- CSS
+				"tsserver", -- TypeScript
 			},
 			-- auto-install configured servers (with lspconfig)
 			automatic_installation = true, -- not the same as ensure_installed
@@ -44,12 +45,14 @@ return {
 
 		mason_tool_installer.setup({
 			ensure_installed = {
+				"eslint_d", -- js linter
+				"pylint", -- python linter
+				"shellcheck", -- bash linter
+				"vale", -- Text, Markdown, Latex linter
+				"black", -- python formatter
+				"isort", -- python formatter
 				"prettier", -- prettier formatter
 				"stylua", -- lua formatter
-				"isort", -- python formatter
-				"black", -- python formatter
-				"pylint", -- python linter
-				"eslint_d", -- js linter
 			},
 		})
 	end,
