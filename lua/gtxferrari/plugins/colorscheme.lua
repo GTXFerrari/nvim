87b13035e733b4c68f039628436abe45f2685912
Jacob Ferrari <jakeferrari93@gmail.com>
2023-11-19 22:38:03 -0800

    change colorscheme to tokyo night

--- lua/gtxferrari/plugins/colorscheme.lua
+++ lua/gtxferrari/plugins/colorscheme.lua
@@ -31,13 +31,13 @@ return {
 
 -- Rose Pine
 -- return {
---   "rose-pine/neovim",
---   name = "rose-pine",
---   lazy = false,
---   priority = 1000,
---   config = function()
---     vim.cmd([[colorscheme rose-pine]])
---   end,
+-- 	"rose-pine/neovim",
+-- 	name = "rose-pine",
+-- 	lazy = false,
+-- 	priority = 1000,
+-- 	config = function()
+-- 		vim.cmd([[colorscheme rose-pine]])
+-- 	end,
 -- }
 
 -- return {
