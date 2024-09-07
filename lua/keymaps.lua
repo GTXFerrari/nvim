83acfcddc6803a2392f86d12d7910127a31800d9
Jacob Ferrari <jakeferrari93@gmail.com>
2024-09-06 20:12:45 -0700

    Update keymaps
    Add my custom keymaps to keymap config

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,9 +1,14 @@
+-- +----------------------------------------------------------------------+
+-- |     ██╗  ██╗███████╗██╗   ██╗███╗   ███╗ █████╗ ██████╗ ███████╗     |
+-- |     ██║ ██╔╝██╔════╝╚██╗ ██╔╝████╗ ████║██╔══██╗██╔══██╗██╔════╝     |
+-- |     █████╔╝ █████╗   ╚████╔╝ ██╔████╔██║███████║██████╔╝███████╗     |
+-- |     ██╔═██╗ ██╔══╝    ╚██╔╝  ██║╚██╔╝██║██╔══██║██╔═══╝ ╚════██║     |
+-- |     ██║  ██╗███████╗   ██║   ██║ ╚═╝ ██║██║  ██║██║     ███████║     |
+-- |     ╚═╝  ╚═╝╚══════╝   ╚═╝   ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝     ╚══════╝     |
+-- +----------------------------------------------------------------------+
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
-
--- Clear highlights on search when pressing <Esc> in normal mode
---  See `:help hlsearch`
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
@@ -16,11 +21,11 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
--- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+--  Disable arrow keys in normal mode
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
@@ -30,6 +35,27 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Resize with arrows
+-- See ':h resize' for more info
+vim.keymap.set('n', '<C-Up>', ':resize +2<CR>', { desc = 'Resize window up' })
+vim.keymap.set('n', '<C-Down>', ':resize -2<CR>', { desc = 'Resize window down' })
+vim.keymap.set('n', '<C-Left>', ':vertical-resize +2<CR>', { desc = 'Resize window left' })
+vim.keymap.set('n', '<C-Right>', ':vertical-resize -2<CR>', { desc = 'Resize window right' })
+
+-- Navigate buffers
+vim.keymap.set('n', '<S-l>', ':bnext<CR>', { desc = 'Cycle next buffer using Shift + l' })
+vim.keymap.set('n', '<S-h>', ':bprevious<CR>', { desc = 'Cycle previous buffer using Shift + h' })
+
+-- Move highlighted text together
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+-- Chmod
+vim.keymap.set('n', '<leader>x', '<cmd>!chmod +x %<CR>', { desc = 'Make a file executable (UNIX)' })
+
+-- Toggle nvim-tree
+vim.keymap.set('n', '<leader>e', '<cmd>NvimTreeToggle<CR>', { desc = 'Toggle nvim-tree' })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
